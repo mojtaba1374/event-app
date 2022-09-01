aee51b10d6ff2603d98b59283efb7317b1eee7ee
mojtaba ameli bidar <mojtabaas95@gmail.com>
2022-09-02 02:42:20 +0430

    test home page for getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,45 @@
+import { useEffect } from 'react';
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/event-list';
+
+import axios from 'axios';
 
 function HomePage() {
 
     const featuredEvents = getFeaturedEvents();
+
+    useEffect(() => {
+        fetch('https://next-test-2155b-default-rtdb.firebaseio.com/events.json')
+            .then(res => res.json())
+            .then(data => console.log(data));
+        
+    }, []);
 
     return(
         <div>
             <EventList items={featuredEvents} />
         </div>
     );
+}
+
+export async function getStaticProps(context) {
+
+    const data = axios('https://next-test-2155b-default-rtdb.firebaseio.com/events.json');
+    const events = await data;
+    console.log(events);
+
+    // fetch('https://next-test-2155b-default-rtdb.firebaseio.com/events.json')
+    //         .then(res => res.json())
+    //         .then(data => console.log(data));
+
+    // const data = await fetch('https://next-test-2155b-default-rtdb.firebaseio.com/events.json');
+    // const events = await data.json();
+
+    return {
+        props: {
+
+        }
+    }
 }
 
 export default HomePage;
